6567ea34165570d8aeb8c71057fac25735e70f5c
shadow-light <noreply@gracious.tech>
2024-07-19 14:40:59 +1000

    .

---
+++ app/src/comp/routes/RouteLegal.vue
@@ -0,0 +1,119 @@
+
+<template lang='pug'>
+
+h1 Legal Guide
+
+p This platform makes setting up a fundraiser easy but it doesn't handle any legal or financial matters for you. You must comply with the laws of where you and your donors reside. This differs by country and can also differ by state/region. It's your responsibility to abide by all relevant laws. To make this easier we offer some non-professional guidance.
+
+div.cs-negative
+    div Disclaimer
+    p The following guidance is offered in good faith but completely without warranty. We are not lawyers or accountants, and providing this guidance does not create a lawyer-client relationship. Follow any of this advice entirely at your own risk.
+
+h2 Australia
+
+p #[strong Summary:]
+ul
+    li #[strong Donations go towards livelihood:] income is taxable but fundraising is simpler
+    li #[strong Donations go towards cause:] income usually not taxable but fundraising is highly regulated
+    li #[strong Donations go towards cause &amp; livelihood:] complicated
+
+hr
+
+p Almost all fundraisers on this platform will be "advancing religion and/or social welfare" which are "charitable" causes in Australia and are under stricter regulation than raising money for "non-charitable" purposes. However, how laws apply vary greatly depending on whether you are raising money for your own personal benefit (livelihood) or not, regardless if the work you actually do is charitable. #[GO(href='https://www.commerce.wa.gov.au/publications/online-crowdfunding-campaigns-social-causes-guidelines') As explained] by the WA State Government:
+
+blockquote
+    p "If a person is collecting on behalf of themselves, the current internal legal position is that this cannot be considered a collection for a charitable purpose, even if the reason for the collection is charitable. The concept of charity is, by definition, at odds with the collection of money for one’s own benefit. In other words, it is impossible for a person to be benevolent or philanthropic towards themselves."
+
+p This is written explicitly regarding law in WA but it is likely other states would be the same, though many of them have not made this clear in writing.
+
+p So it matters a great deal whether you declare your fundraiser as going towards "Livelihood", "Cause", or "Livelihood &amp; Cause" when creating it on our platform.
+
+div.cs-negative
+    div Always keep accurate records
+    p Regardless of what type of fundraiser you set up, you will usually be legally required to retain accurate records of donations. This is also a requirement of our platform. We recommend creating a separate bank account exclusively for collecting donations.
+
+
+h3 Funds go towards "Livelihood &amp; Cause"
+
+p This option is complex and you will probably be better off choosing one or the other in Australia. Some of the income will be taxable and some won't. And you #[em will] be subject to fundraising regulations by states. We can't give guidance on this because it will vary greatly depending on the ministry.
+
+p #[strong Alternatives include:]
+ul
+    li Declaring that donations only go towards "Livelihood" and paying any ministry expenses out of your own income.
+    li Doing ministry under an existing non-profit or church, which pays you a stipend to support you and is also registered to fundraise for charitable purposes.
+
+
+h3 Funds go towards "Livelihood"
+
+p As Christians, we should never be in ministry for the money, which is why many people will prefer to raise money for a cause/ministry rather than themselves. However, it is #[router-link(to='/official/compliance') biblical and appropriate] to raise money for yourself so you can devote your time to ministry, which is what most missionaries and pastors do already. It also turns out to be simpler legally in Australia than raising for a cause.
+
+h4 Tax
+
+p Any income that goes to you personally (less expenses) is taxable. The ATO has #[GO(href='https://www.ato.gov.au/law/view/document?DocID=ITR/IT2674/NAT/ATO/00001&PiT=99991231235958') made a ruling] specifically for those in ministry. For taxation purposes, ministry is considered to be "carrying on a business", and they even refer to evangelism as being "a business of evangelism". Even if donations to you are entirely voluntary.
+
+p Individuals should declare donations as #[GO(href='https://www.ato.gov.au/businesses-and-organisations/starting-registering-or-closing-a-business/starting-your-own-business/business-structures-key-tax-obligations#ato-Soletrader') sole trader/business income]. You do not need to actually set anything else up as a "business". However, if you earn $75,000/year or more you will need to get an ABN and register for GST (even if you don't actually charge anyone anything).
+
+p If ministry ends up being your main occupation you should consider joining or starting a non-profit organisation, as there are #[GO(href='https://www.ato.gov.au/law/view/document?LocID=%22TXR%2FTR20193%2FNAT%2FATO%22&PiT=99991231235958') some tax benefits] for religious practitioners. However, starting your own non-profit is a lot of work and will take time away from your ministry, so it is not advised to attempt it for small initiatives. A better option could be to work under an existing church or non-profit.
+
+h4 Fundraising
+
+p Raising money for yourself is likely to not be subject to regulation in most states. The government rightly regulates charitable fundraising so that money donated towards causes doesn't just end up in someone's personal bank account. But as Christians it is appropriate to give towards someone's personal income so they can devote their time to ministry.
+
+p The important part is making sure donors understand they are giving to your personal income rather than to a particular cause. We make this clear when you select the "Livelihood" option when creating a fundraiser. You can still fund ministry expenses out of your own personal income, but this must not be suggested to be what donors are giving towards.
+
+div.cs-negative
+    div Illegal
+    p Susan writes books to edify the church. She encourages people to donate to help fund her latest book on discipleship so she can give it away for free. Many people are excited about the latest book and she raises $25,000.
+    p [Susan has informed donors that their giving goes towards the charitable cause of giving her latest book away for free. She needs a fundraising license to be able to do this.]
+
+div.cs-positive
+    div Likely to be legal
+    p Susan writes books to edify the church. She encourages people to donate so she can keep writing books and give them away. She has selected "Livelihood" when creating her fundraiser so it is clear that any donations go towards her personal income. She shares about her latest book on discipleship that she plans to write but doesn't claim donations go specifically towards that. Many people believe in her work and she raises $25,000. She declares it as business income on her tax return.
+    p [Susan has made it clear that all donations go towards her personal income and not a charitable purpose in the eyes of the law.]
+
+
+
+h3 Funds go towards "Cause"
+
+h4 Tax
+
+p You will not usually need to pay any tax on money received since we already require your fundraiser to be non-commercial and non-profit. For more information, see the #[GO(href='https://www.ato.gov.au/businesses-and-organisations/income-deductions-and-concessions/income-and-deductions-for-business/in-detail/crowdfunding') ATO's guide on crowdfunding].
+
+h4 Fundraising
+
+p Fundraising is regulated in Australia at a state level. It may be illegal to request donations for a charitable purpose if you do not have a license to do so. It is currently (July 2024) very complicated and the federal government is trying to get all the states to #[GO(href='https://www.communitydirectors.com.au/articles/states-slow-to-act-on-fundraising-reform') agree on a common standard].
+
+p In general, you must register to fundraise if you raise:
+
+ul
+    li NSW &mdash; #[GO(href='https://www.nsw.gov.au/money-and-taxes/charitable-fundraising/charitable-fundraising-guidelines/authority-to-fundraise-nsw#toc-who-is-exempt-from-holding-an-authority') more than $15,000/year]
+    li QLD &mdash; #[GO(href='https://www.qld.gov.au/law/laws-regulated-industries-and-accountability/queensland-laws-and-regulations/associations-charities-and-non-for-profits/charities-and-fundraising/starting-an-appeal-for-fundraising-support/fundraise-for-community-purpose') any amount]
+    li VIC &mdash; #[GO(href='https://www.consumer.vic.gov.au/clubs-and-fundraising/fundraisers/registration/exemptions-from-registration') more than $20,000/year]
+    li SA &mdash; #[GO(href='https://www.sa.gov.au/topics/family-and-community/community-organisations/not-for-profit/charities') any amount]
+    li WA &mdash; #[GO(href='https://www.commerce.wa.gov.au/publications/online-crowdfunding-campaigns-social-causes-guidelines') any amount]
+    li TAS &mdash; #[GO(href='https://www.cbos.tas.gov.au/topics/clubs-fundraising/charities/apply-to-collect') any amount]
+    li NT &mdash; #[GO(href='https://www.acnc.gov.au/fundraising-northern-territory') no registration required]
+    li ACT &mdash; #[GO(href='https://www.accesscanberra.act.gov.au/business-and-work/associations-co-ops-and-charities/charitable-collection-licences') more than $15,000/year]
+
+div(class='mt-4')
+    VBtn(color='' variant='tonal' size='small' href='https://content.nfplaw.org.au/wp-content/uploads/2023/06/Guide-to-fundraising-laws-in-Australia.pdf') Detailed Guide
+
+p In most cases registration will be complicated and require you to setup structures similar to a charity. So if registration is required for your situation then it may be simpler to either join an existing non-profit or charity, or start your own if necessary.
+
+h2 Other countries
+
+p We don't have any advice for other countries yet. If you have carefully researched the laws for your country you can send us a summary that we'll consider adding to this page.
+
+</template>
+
+
+<script lang='ts' setup>
+
+
+</script>
+
+
+<style lang='sass' scoped>
+
+
+</style>
