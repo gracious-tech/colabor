ef79a1b9a92fcae05176cdc6e8505df5527b341e
shadow-light <noreply@gracious.tech>
2025-03-03 21:51:21 +1100

    .

---
+++ app/src/comp/routes/RouteBitcoin.vue
@@ -0,0 +1,64 @@
+
+<template lang='pug'>
+
+h1 Why Accept Bitcoin?
+
+p We support donating via Bitcoin on this platform because it is:
+
+ol
+    li Lower in transaction fees
+    li Easier to transfer internationally
+    li More likely to retain its value
+
+p We'll explain each of these in turn, but first let's clarify what this is #[strong #[em not]] about.
+
+h2 Misconceptions
+
+ul
+    li #[strong Bitcoin is bad for the environment:] Bitcoin mining does require a lot of energy, however, so do cars and the credit card networks. The question is whether the energy use is justified for the value it produces. Bitcoin miners do get rewarded with more bitcoin but the reason they are rewarded is because they are processing transactions. It is not a wasteful activity any more than processing credit card payments is wasteful.
+    li #[strong Bitcoin is akin to gambling:] Bitcoin's price has been volatile and some people do trade it to try to get-rich-quick, but that doesn't mean everyone does. People trade the USD and stocks to try to get rich too, but that doesn't mean they can't be used responsibly.
+    li #[strong Bitcoin is used by criminals:] Bitcoin is attractive for its anonymity but its transaction amounts are completely public, so it is not always easier to use for criminal activity. Ordinary cash on the other hand can be exchanged anonymously and without any record of transaction.
+    li #[strong Cryptocurrencies are scams:] Yes, some of them are. We are only talking about Bitcoin and do not recommend any other cryptocurrency at this time. Zimbabwe's dollar was poorly managed and is now worthless, but that does not mean the USD is worthless. Likewise, Bitcoin must be judged on its own merits as it has several advantages not present in many other cryptocurrencies.
+
+p The point here is not to promote Bitcoin but to merely show that there are financial and ethical tradeoffs with all forms of currency and assets. Believers are free to have their own opinions about how to best steward finances, but we hope to make it clear that Bitcoin is a reasonable option when used responsibly.
+
+
+h2 Why Bitcoin is Good for Funding Ministry
+
+p Bitcoin may well make some people rich (or poor when traded irresponsibly) but that is not why we are recommending it. Rather than investing in Bitcoin, we're talking about funding ministry with it. Sending moderate amounts to people in ministry so they can have a livelihood and cover whatever expenses they need to.
+
+p While Bitcoin might have some uses in funding ministry domestically, the focus here is primarily on funding ministry internationally, which is often difficult using regular currencies.
+
+
+h3 Lower in transaction fees
+
+p Donating money usually involves transaction fees. Using a card will usually involve fees of 1.5-3.5% or 3-6% for international donations. Services like PayPal appear to be low cost but they actually convert currencies ~3% lower than they should be. Direct bank transfers are usually the cheapest way of donating but they are not available in all countries and incur very high fees internationally.
+
+p Bitcoin on the other hand involves a fee that is not relative to the amount being transferred (not a %). The fee varies based on how many people are using the network but is around $1-10 USD when ignoring irregular spikes. This makes it ideal for one-off or infrequent donations, since the cost is per transaction. The fewer transactions, the fewer fees.
+
+p So if you wanted to donate $1,000 to a ministry in another country it would usually cost $30-60 in fees or loss during currency conversion. Whereas a fee in bitcoin could be just a few dollars.
+
+p It should be noted that there'll usually be a fee when converting from a domestic currency to Bitcoin, but these can be as low as 0.1-1%. The value of bitcoin is also generally increasing so it may be even more valuable when it is time to send it, offsetting any exchange fees.
+
+
+h3 Easier to transfer internationally
+
+p Many believers live in countries where it is very difficult to receive money internationally. They may not be allowed to open a PayPal account or receive funds from other countries. Since Bitcoin is not controlled by anyone, anyone in the world can receive it simply by installing an app on their phone.
+
+p They can then use Bitcoin to pay for many online services, such as running a website for their ministry. Most countries will have an exchange where they can turn their Bitcoin into local currency to pay for everyday expenses. They may also be able to exchange it via another country's exchange that allows bank transfers to their own. They can also simply hold onto it so that their savings increase and they can eventually exchange it to buy a home or to get them through economic hardship.
+
+h3 More likely to retain its value
+
+p If donations pool in a ministry's account without earning interest then they will gradually decrease in value due to inflation. Even when earning interest, the rate may still be less than inflation.
+
+p After 15 years, Bitcoin has proven to retain its value, far exceeding inflation and debasement that other currencies have been subject to. It has had a very volatile history, which is to be expected for something new. However, it has now gone mainstream with millions of users, a trillion dollar market capitalization, support from large companies, and is even an official currency of El Salvador. So it is unlikely its value will be as volatile going forward as in the past.
+
+p This is especially useful for believers in countries with unstable economies, that they might have access to a currency that will not waste away before they have had a chance to use it.
+
+h2 Conclusion
+
+p Bitcoin can be a valuable means of funding ministry, especially when sending funds internationally. It can be virtually impossible for believers in certain countries to get the support they need from believers in places better off. Bitcoin has changed this for the better.
+
+p We do not suggest all ministries start using Bitcoin. We merely argue that it may be a better option for some, and should be considered as a public good when used appropriately.
+
+</template>
