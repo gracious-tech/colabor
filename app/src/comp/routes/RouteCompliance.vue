32ea344927ce4db0be3bbe01421cabfe71b7b13a
shadow-light <noreply@gracious.tech>
2025-03-03 23:59:13 +1100

    .

--- app/src/comp/routes/RouteCompliance.vue
+++ app/src/comp/routes/RouteCompliance.vue
@@ -4,27 +4,25 @@
 h1 Character Before Compliance
 
 
-p In the pursuit of integrity, Christians have added numerous layers of accountability for receiving donations. Those in ministry are expected to be part of a reputable organisation with non-profit regulations, multiple board members, independent audits, finance officers, and financial stewardship accreditation.
-
-p Those are often good things but they also introduce a large amount of admin, which is a problem for those starting new ministries or running only small initiatives. Someone starting a ministry can easily find they spend more time dealing with admin then actually doing ministry, and have a substantial amount of donations consumed by admin expenses.
-
+p When it comes to receiving donations, Christians expect many layers of accountability to ensure they are used appropriately. Those in ministry are expected to be part of a reputable organisation with non-profit regulations, multiple board members, independent audits, finance officers, and sometimes even #[a(href="https://ministrywatch.com/how-ministrywatch-arrives-at-a-transparency-grade/") financial stewardship accreditation].
+p Those are often good things but they also introduce a large amount of admin, which is a problem for those starting new ministries or running only small initiatives. Someone starting a ministry can easily find they spend more time dealing with admin than actually doing ministry, and have a substantial amount of donations consumed by admin expenses.
 p
     em
         strong We argue that the level of admin and accountability expected should be proportional to the size of the ministry.
 
 p As such, we encourage directly supporting individuals in ministry with personal income, as long as the amount doesn't exceed the wages for a reasonable living.
 
-h2 Accountability has been emphasised over integrity
+h2 Accountability has been emphasized over integrity
 
 p Accountability can be a helpful tool but it is no substitute for genuine integrity. Someone can be completely righteous in their actions without any accountability, while someone can have all the accountability in the world and still fall into sin.
 
-p At a theological level, we've emphasised trusting policies and safeguards rather than ensuring people are genuinely trustworthy. Such policies and safeguards are usually good things but they do not get to the heart of the matter.
+p At a theological level, we've emphasized trusting policies and safeguards rather than ensuring people are genuinely trustworthy. Such policies and safeguards are usually good things but they do not get to the heart of the matter.
 
 p We do not want people in ministry who love money but are restrained from acting out their sinful desires merely because of some policies that make it difficult to. We don't want people who only steward resources well because there is accountability in place. We want people who are genuinely godly and trustworthy, who will do the right thing whether someone is watching or not.
 
 div.cs-negative
-    div Wasting time
-    p Peter is an app developer and has some ideas for evangelistic apps. He wonders if people will give money to help fund them but a friend says it is wrong to receive money directly and he should start a non-profit organisation instead. He spends many weeks researching non-profit organisations and how to set them up, then even longer finding people to be part of his board. Some of the board members don't really understand apps while others do but aren't very mature in their faith. At the end of the first year Peter realises he's spent more time setting up his non-profit than he has actually making apps.
+    div Wasting time (all examples are semi-fictional)
+    p Peter does cross-cultural evangelism in his suburb amongst Buddhists. He wonders if people will give money to help fund his ministry but a friend says it is wrong to receive money directly and he should start a non-profit organisation instead. He spends many weeks researching non-profit organisations and how to set them up, then even longer finding people to be part of his board. Some of the board members don't really understand ministry to Buddhists while others do but aren't very mature in their faith. At the end of the first year Peter realizes he's spent more time setting up his non-profit than he has actually sharing the gospel with Buddhists.
 
 
 h2 Supporting causes AND people
@@ -51,11 +49,11 @@ h3 Organisations ensure funds are kept track of
 
 p The most basic form of accountability is actually keeping track of how much money is coming in. Organisations usually have someone dedicated to this task, carefully keeping account of all income and expenditure.
 
-p However, there is no reason why an individual cannot do this themselves. Small ministries often have so few transactions that they can fit on a single page, so extensive accounting is not necessary.
+p However, there is no reason why an individual cannot do this themselves. Small ministries often have so few transactions that they can fit on a single page, so extensive accounting is not necessary. Anyone can set up a new online bank account for receiving donations within minutes, and auditing the account can be as simple as forwarding an annual bank statement to a church pastor or lay leader for a degree of accountability.
 
 div.cs-negative
     div Excessive accounting wastes money
-    p Simon helps the poor in his neighbourhood. He's heard of financial abuse in other charities and wants to make sure his work is above reproach, so he starts a non-profit and does an independent audit each year. He manages to raise $30,000 in donations, but $5,000 of that is spent on the independent audit each year, and another $5,000 for other admin, leaving only 66% for giving to the poor.
+    p Simon helps the poor in his neighborhood. He's heard of financial abuse in other charities and wants to make sure his work is above reproach, so he starts a non-profit and does an independent audit each year. He manages to raise $30,000 in donations, but $5,000 of that is spent on the independent audit each year, and another $5,000 for other admin, leaving only 66% for giving to the poor.
 
 
 h3 Organisations ensure secure means of payment
@@ -66,7 +64,7 @@ p While some organisations claim that card payments are the best way to donate, 
 
 div.cs-negative
     div Inefficient means of payment
-    p Jess has been told that getting people to give regularly is the most stable way to fund a ministry and she should join an organisation as they support paying by credit card. One of her supporters asks if they can just do a bank transfer to avoid the card surcharges. But her organisation tells her the supporter might drop off if they can't secure their card details. Jess regularly loses 1-2% of each donation due to card surcharges.
+    p Jess has been told that getting people to give regularly is the most stable way to fund a ministry and she should join an organisation as they support paying by credit card. One of her supporters asks if they can just do a bank transfer. But her organisation tells her the supporter might drop off if they can't secure their card details. The organisation regularly loses 1-2% for each donation due to card surcharges and these costs get passed on to Jess through the membership fee she pays each year.
 
 h3 Organisations ensure funds are used appropriately
 
@@ -76,18 +74,19 @@ p Most small ministries are supported by those who themselves see the fruit that
 
 div.cs-negative
     div What will people give to
-    p Paul is working on aligning the original manuscripts of Scripture with modern translations. This will eventually allow richer study of Scripture for people all around the world. It's gruelling work and there aren't many interesting updates to share. Meanwhile another organisation that relies on his work has a large marketing budget and gets a lot of donations as a result. They share repetitive updates on how many people are being reached with the gospel, even though Paul knows this isn't a big part of what they do.
+    p Paul is working on matching the data of original manuscripts to modern translations of Scripture. This will eventually allow richer study of Scripture for people all around the world. It's grueling work and there aren't many interesting updates to share. Meanwhile another organisation that relies on his work has a large marketing budget and gets a lot of donations as a result. They adopt Paul's work into their own organisation and share repetitive updates on how many people are being reached with the gospel, even though Paul knows this isn't a big part of what they do.
 
 
 h3 Organisations ensure the ministry is non-profit
 
-p Some Christian organisations are actually for-profit and it can be hard to know which as they rarely make this obvious. Regarding organisations that are set up as non-profits, it's important to realise this is a legal distinction and not an entirely biblical one. A non-profit organisation can only use its funds to further the cause it was set up for, but there are few other restrictions and many executives personally profit through high salaries.
+p Some Christian organisations are actually for-profit and it can be hard to know which as they rarely make this obvious. Regarding organisations that are set up as non-profits, it's important to realize this is a legal distinction and not an entirely biblical one. A non-profit organisation can only use its funds to further the cause it was set up for, but there are few other restrictions and many executives personally profit through #[a(href="https://ministrywatch.com/100-highly-paid-ministry-executives-2024/") high salaries]. Organisations rarely disclose how much they pay staff, though some governments force them to (such as the US), yet many still #[a(href="https://ministrywatch.com/ministry-spotlight-compassion-international/") find ways around this].
 
-p It is common practice for Christian organisations to compare to each other when setting executive pay, or even compare to the secular world. In other words, the policy is "we'll do what everyone else is doing". As a result there are Christian CEOs that receive over $400k USD/year (Compassion), $800k (Samaritan's Purse), or even $7 million (Inspiration Ministries). While the latter is associated with false teachers, it still goes to show that it is legal to pay incredibly high salaries for non-profit executives.
+p It is common practice for Christian organisations to compare to each other when setting executive pay, or even compare to the secular world. In other words, the policy is “we'll do what everyone else is doing.” As a result there are Christian executives that receive over $500k USD/year (#[a(href="https://projects.propublica.org/nonprofits/organisations/941514010") Bible Study Fellowship]), $800k (#[a(href="https://projects.propublica.org/nonprofits/organisations/581437002") Samaritan's Purse]), or even $9 million (#[a(href="https://projects.propublica.org/nonprofits/organisations/570931093") Inspiration Network]). While the latter is associated with false teachers, it still goes to show that it is legal to pay incredibly high salaries for non-profit executives.
+p So if an individual were to simply disclose how much they personally receive from donations then they would already be more accountable in that regard than almost all Christian non-profit organisations.
 
 div.cs-negative
-    div Commercialising ministry
-    p Clare dedicates a lot of her time to writing worship songs. She has a part-time job put she's still eating into her savings. Someone suggests asking for donations but that sounds weird, as it seems to be only something churches and missionaries do. So she goes with a commercial publisher instead but is careful to pick one that is non-profit. Churches pay her royalties whenever they sing her songs but CCLI takes a cut and so does her publisher, leaving her with less than 50%. She gets by, but no one is allowed to sing her songs unless they pay because they are copyrighted.
+    div Commercializing ministry
+    p Clare dedicates a lot of her time to writing worship songs. She has a part-time job but she's still eating into her savings. Someone suggests asking for donations but that sounds weird, as it seems to be only something churches and missionaries do. So she goes with a commercial publisher instead but is careful to pick one that is non-profit. Churches pay her royalties whenever they sing her songs but the secular for-profit company CCLI [takes a cut and so does her publisher](https://sellingjesus.org/articles/worship-tax), leaving her with less than 50%. She gets by, but no one is allowed to sing her songs unless they pay because they are copyrighted.
 
 
 
@@ -105,28 +104,37 @@ div.cs-negative
 h2 A reasonable alternative for small ministries
 
 
-p As we've seen, organisations can lower risk with the accountability they provide, but they do not eliminate it and they lower it at a cost; A cost that is not always appropriate for low risk scenarios. We've also seen that there is nothing inherently wrong with directly giving someone money, and it is much more efficient in terms of both time and expenses for small ministries. There is not currently a culture of giving to all the various forms of ministry possible and so many have instead turned to commercialising their ministry to get by.
-
-p We propose that Christians should give wholeheartedly to small ministries without expecting the same level of admin as organisations.
+p There are some truly wonderful Christian organisations which carefully steward the money donated to them and do a great job of keeping themselves and their staff accountable. But they do not eliminate risk and there is a cost to the accountability they provide; a cost that is not always appropriate for small ministries.
+p There is also nothing inherently wrong with directly giving someone money, and it is much more efficient in terms of both time and expenses for small ministries. There is not currently a culture of giving to all the various forms of ministry possible and so many people have instead turned to commercializing their ministry instead. Many well known Christian leaders are already selling their teaching via books and other media, with little accountability on how much they are earning and with the money going directly into their personal accounts.
+p We propose that Christians should give wholeheartedly to small ministries without expecting the same level of admin and accountability as organisations. Simple measures can provide an appropriate level of accountability that even exceeds that of many non-profit organisations (in respect to disclosing personal remuneration and #[a(href="https://sellingjesus.org") not charging for ministry]).
+p It would not be appropriate to propose a set of specific rules regarding amounts and practices, as that would be to go beyond what Scripture teaches (Mark 7:7). Individual platforms and churches should have their own set of standards they deem appropriate. For example, to require everyone to use a separate bank account would be an unnecessary burden to believers in countries that are still primarily cash economies, and a modest income in the US could be a life of luxury in another country. Instead, the following guidelines are offered as a mere #[em suggestion] on what might be reasonable for many in the West:
 
 p
     strong Responsibility of fundraisers:
 ul
-    li Keep track of donations
-    li Raise a sum that is reasonable (for your circumstances, not for an equivalent career)
-    li Encourage donors to give to other ministries when sufficiently funded
+    li Ensure money is going towards ministry
+        ul
+            li Be honest with supporters and keep them informed of significant changes
+            li Raise an appropriate amount (for a livelihood, not a career)
+            li #[a(href="https://copy.church/licenses/") Freely give] any ministry resources you produce
+    li Keep track of donations (such as by opening a new bank account for them)
+        ul
+            li Send the annual bank statement to a church pastor or lay leader for accountability
+            li Report donations in your tax return (for countries that require it)
+    li Start or join a non-profit if your ministry grows beyond a single person/family
 
 p
     strong Responsibility of donors:
 ul
     li Ensure you trust the people fundraising (either personally or based on the fruit of their ministry)
+    li Stop donating if you are no longer comfortable with their beliefs, lifestyle, or practices
 
-p All fundraisers should promise to carefully steward what is given to them and to be honest with how much they are raising. While there are less legal means of accountability and Scripture tells us that some will raise money for impure motives (1 Tim 6:5), we should not let that dissuade us from supporting the many people who serve the Lord with integrity. It is biblical to not be overly concerned about bringing people to justice ourselves (Prov 20:22, Rom 12:19, 1 Cor 6:7), the Lord will deal with those who are servants of money in his own time.
+p This proposal will by no means ensure that fraud does not take place, as Scripture affirms that some will raise money for impure motives (1 Tim 6:5 ). But as explained already, nonprofits can't completely guard against this either. While donations to individuals may involve less legal protections, we should not let that dissuade us from supporting the many people who serve the Lord with integrity. It is biblical to not be overly concerned about bringing people to justice ourselves (Prov 20:22, Rom 12:19, 1 Cor 6:7), as the Lord will deal with those who are servants of money in his own time (John 12:6). We should focus on ensuring that those who do go out on behalf of the Name are well supported so we may be fellow workers for the truth with them (3 John 1:8).
 
 
 div.cs-positive
     div
-    p Clare dedicates Fridays to writing worship songs and hopes to raise $15,000 USD/year to help do this. She opens an online savings account to receive donations into and occasionally transfers them into her transactions account. At the end of the year she simply checks the income total of the donations account to know how much she has received. She raises more than expected and so she dedicates even more time to her song writing, adjusting her goal to $30,000. That goal too is reached and so she closes her fundraising page and keeps her existing donors updated on her ministry. She releases all her songs into the public domain so that churches can sing them free of charge and without needing a license.
+    p Belle dedicates Fridays to writing worship songs and hopes to raise $15,000 USD/year to help do this. She opens an online savings account to receive donations into and occasionally transfers them into her transactions account. At the end of the year she checks the income total of the donations account to know how much she has received and forwards the bank statement to an elder at her church who's offered to keep her accountable. She eventually reaches her goal and closes her fundraising page, keeping her existing donors updated on her ministry. She releases all her songs into the public domain so that churches can sing them without needing to pay for a license to do so.
 
 
 
