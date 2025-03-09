137037df22b2066d9f6333ec5b5469579b76e551
shadow-light <noreply@gracious.tech>
2025-03-10 10:57:07 +1100

    .

--- app/src/comp/routes/RouteGuideSupporters.vue
+++ app/src/comp/routes/RouteGuideSupporters.vue
@@ -6,6 +6,13 @@ h1 Guide For Supporters
 
 h2 Why should I give directly to someone?
 
+h2 Can I give towards a particular activity of a fundraiser?
+
+p You can only give towards the entire fundraiser, not specific activities within that fundraiser. But you can show your appreciation of a particular activity by selecting it when donating.
+
+p The reason for this is that it's an administrative burden to divide donations by specific causes, and it can also limit the ability for a ministry to function if some parts of it end up being more funded than others. In order to avoid burdening ministries in these ways, we don't support giving towards a particular activity.
+
+
 h2 Why is Bitcoin accepted?
 
 </template>
