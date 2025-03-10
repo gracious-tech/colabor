4f099eb81f0775fbe85c704649b025c8454a13a0
shadow-light <noreply@gracious.tech>
2025-03-10 14:16:12 +1100

    .

---
+++ app/src/comp/dialogs/parts/DialogDonateOption.vue
@@ -0,0 +1,18 @@
+
+<template lang='pug'>
+
+VCard(:title="title")
+
+</template>
+
+
+<script lang='ts' setup>
+
+const props = defineProps<{title:string, body:string}>()
+
+</script>
+
+
+<style lang='sass' scoped>
+
+</style>
