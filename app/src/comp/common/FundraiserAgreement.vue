4083a0ac4599850ecc7d135fedc67c35a05ef089
shadow-light <noreply@gracious.tech>
2025-03-10 14:02:16 +1100

    .

--- app/src/comp/common/FundraiserAgreement.vue
+++ app/src/comp/common/FundraiserAgreement.vue
@@ -1,11 +1,11 @@
 
 <template lang='pug'>
 
-ol
+ol.root
     li I am responsible for my own use of this platform:
         ol
             li I will obey any relevant tax and fundraising laws
-            li I will not hold this platform's creator (Gracious Tech) responsible for any legal issues related to my use of this platform
+            li I will not hold this platform's creator (Gracious Tech) responsible for any legal or financial issues related to my use of this platform
     li I will keep track of donations:
         ol
             li I will keep track of how much I receive
@@ -13,22 +13,30 @@ ol
             li I will notify my supporters of significant changes in circumstances
     li I will follow this platform's fundraising rules:
         ol
-            li I will not charge for ministry
+            li I will not charge for access to ministry
             li I will #[GO(href='https://copy.church') free all works] produced from copyright restrictions
-            li I will not raise more than $200,000 USD per year
             li I will not seek a personal income that is excessive for my circumstances
-            li I will not fundraise if I have personal assets exceeding $400,000 USD (excluding a single home of residence, or savings for one)
-    li I affirm:
+            li I will pause the fundraiser if I exceed my stated goal (keeping existing donors)
+            li I will pause the fundraiser if my income exceeds $200,000 USD per year or my personal assets exceed $500,000 USD (excluding a single home of residence, or savings for one)
+    li In terms of my beliefs, I affirm:
         ol
-            li The Apostle's Creed
+            li The Apostles' Creed
             li There is one God in three persons: the Father, the Son, and the Holy Spirit
             li The Bible is our ultimate authority on knowing God and his will
             li Gracious Tech decides if a fundraiser is in contradiction to Scripture or not
     li I will stop accepting donations and disable my fundraising page if:
         ol
             li I am no longer doing ministry
-            li I exceed my stated income cap
-            li I exceed my stated assets cap
             li Changes in my beliefs or practices contradict this agreement
 
 </template>
+
+
+<style lang='sass' scoped>
+
+.root > li
+    font-weight: bold
+    & > *
+        font-weight: normal
+
+</style>
