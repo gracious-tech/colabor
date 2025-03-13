bc968de9d58b450d9705a6b6b03f4dc5f3c6a2a9
shadow-light <noreply@gracious.tech>
2025-03-13 16:18:33 +1100

    .

---
+++ app/src/comp/dialogs/parts/DialogDonateNext.vue
@@ -0,0 +1,20 @@
+
+<template lang='pug'>
+
+VBtn(color='secondary')
+    slot Next
+    template(#append)
+        AppIcon(name='arrow_forward')
+
+</template>
+
+
+<script lang='ts' setup>
+
+
+</script>
+
+
+<style lang='sass' scoped>
+
+</style>
---
+++ app/src/comp/dialogs/parts/DialogDonatePrev.vue
@@ -0,0 +1,20 @@
+
+<template lang='pug'>
+
+VBtn(color='' variant='tonal')
+    template(#prepend)
+        AppIcon(name='arrow_back')
+    slot Prev
+
+</template>
+
+
+<script lang='ts' setup>
+
+
+</script>
+
+
+<style lang='sass' scoped>
+
+</style>
