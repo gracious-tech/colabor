49e903558043a264454c3bab0ec239183488e2cb
shadow-light <noreply@gracious.tech>
2025-03-15 00:35:02 +1100

    .

--- app/src/comp/routes/RouteGuideSupporters.vue
+++ app/src/comp/routes/RouteGuideSupporters.vue
@@ -6,6 +6,13 @@ h1 Guide For Supporters
 
 h2 Why should I give directly to someone?
 
+p With the church's increased focus on organizational structures, it has become rare to support someone in ministry by directly giving to them. Yet this was how ministry was funded in the early church. Paul did not set up a non-profit for his ministry, people gave directly to him (Phil 4:15-18). Likewise, John encouraged believers to directly support evangelists (3 John 1:5-8).
+
+p Not only were people in ministry supported, but their families were provided for as well (1 Cor 9:5). The church has kept up this practice in regards to missionaries, and sometimes in regards to pastors. If one spouse wishes to do mission, the church does not send them out alone, the whole family is sent out together as one unit.
+
+p It is of course also biblical to support causes (Rom 15:25-27) rather than just people. The point is that Christians should not shy away from directly supporting those engaged in ministry and should not limit them to a single cause. Someone may be involved in a variety of different ministries, but only funding them for a particular cause can restrict the good they are able to do in other areas.
+
+
 h2 Can I give towards a particular activity of a fundraiser?
 
 p You can only give towards the entire fundraiser, not specific activities within that fundraiser. But you can show your appreciation of a particular activity by selecting it when donating.
