7b44946ac5effa7a5f70779e1a8d057f626f31aa
shadow-light <noreply@gracious.tech>
2025-03-15 00:27:19 +1100

    .

--- app/src/comp/routes/RouteGuideSupporters.vue
+++ app/src/comp/routes/RouteGuideSupporters.vue
@@ -13,8 +13,6 @@ p You can only give towards the entire fundraiser, not specific activities withi
 p The reason for this is that it's an administrative burden to divide donations by specific causes, and it can also limit the ability for a ministry to function if some parts of it end up being more funded than others. In order to avoid burdening ministries in these ways, we don't support giving towards a particular activity.
 
 
-h2 Why is Bitcoin accepted?
-
 </template>
 
 
