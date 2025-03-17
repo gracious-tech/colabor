7392ffc69f33048c54bf08fe8dee653585af07ef
shadow-light <noreply@gracious.tech>
2025-03-18 02:06:40 +1100

    .

--- app/src/comp/routes/parts/RouteRootSlideshow.vue
+++ app/src/comp/routes/parts/RouteRootSlideshow.vue
@@ -6,7 +6,7 @@ div.slideshow(ref='slideshow_div')
     div.text A fundraising platform for Christian…
 
     div.line(ref='line_div')
-        div.item(v-for='person of people')
+        div.item(v-for='person of people' :key='person')
             div.text {{ person }}.
             img(:src='`/_assets/slideshow/${person}.jpg`')
 
@@ -16,6 +16,8 @@ div.slideshow(ref='slideshow_div')
 <script lang='ts' setup>
 
 import {onBeforeUnmount, onMounted, ref} from 'vue'
+
+import {debounce} from '@/services/utils'
 
 
 const line_div = ref<HTMLDivElement>()
@@ -33,30 +35,36 @@ people = people.map(value => ({value, sort: Math.random()}))
 people.push('ministry')
 
 const display_line_end = () => {
-        const container_width = slideshow_div.value!.getBoundingClientRect().width
-        const item_width = container_width / 100 * 50
-        let margin = (people.length - 1) * item_width  // Sum width of all items except last
-        const img_height = item_width / 16 * 9
-        line_div.value!.querySelectorAll('img').forEach((img, i) => {
-            const last = i === people.length -1
-            // Give last width of full container so ends showing only last item
-            img.style.width = (last ? container_width : item_width) + 'px'
-            img.style.height = img_height + 'px'
-        })
-        line_div.value!.style.marginLeft = '-' + margin + 'px'
+    if (!line_div.value){
+        return  // Unmounted already
     }
+    const container_width = slideshow_div.value!.getBoundingClientRect().width
+    const item_width = container_width / 100 * 50
+    const margin = (people.length - 1) * item_width  // Sum width of all items except last
+    const img_height = item_width / 16 * 9
+    line_div.value.querySelectorAll('img').forEach((img, i) => {
+        const last = i === people.length -1
+        // Give last width of full container so ends showing only last item
+        img.style.width = (last ? container_width : item_width) + 'px'
+        img.style.height = img_height + 'px'
+    })
+    line_div.value.style.marginLeft = '-' + margin + 'px'
+}
 
-let observer:ResizeObserver
+
+// Get debounced version so can add/remove event listener
+const debounced_display_line_end = debounce(display_line_end)
 
 onMounted(() => {
+    // Can't calc properly till after DOM fully mounted
     setTimeout(display_line_end, 1)
-    // line_div.value!.style.transitionProperty = 'none'
-    observer = new ResizeObserver(display_line_end)
-    observer.observe(slideshow_div.value!)
+    // Need to recalc if window size changes
+    // WARN Was using ResizeObserver but it caused infinite loop issues due to triggering resize
+    self.addEventListener('resize', debounced_display_line_end)
 })
 
 onBeforeUnmount(() => {
-    observer.disconnect()
+    self.removeEventListener('resize', debounced_display_line_end)
 })
 
 </script>
