2853276ba98f42d50750317279f1f76ff5fd7715
shadow-light <noreply@gracious.tech>
2025-03-25 00:53:15 +1100

    og meta handling

--- functions/src/ogimage.ts
+++ functions/src/ogimage.ts
@@ -18,7 +18,20 @@ export const replace_ogimage = onRequest((request, response) => {
     const img_url = `/dev/${fund_id}/header.jpg`
 
     // Replace default og:image with fund's header image
-    const resp_html = index_html.replace('/social.jpg', img_url)
+    let resp_html = index_html.replace('/social.jpg', img_url)
+
+    // Identify existing title/desc so can more easily replace
+    const old_title = /<title>(.*?)</.exec(resp_html)?.[1]
+    const old_desc = /name=['"]description['"] content="(.*?)"/.exec(resp_html)?.[1]
+
+    // Replace meta with generic text that includes fund id
+    // This is good enough for a link preview and still doesn't require a slower db request
+    if (old_title){
+        resp_html = resp_html.replaceAll(old_title, `Support ${fund_id}`)
+    }
+    if (old_desc){
+        resp_html = resp_html.replaceAll(old_desc, "Learn more")
+    }
 
     // Send response
     response.status(200).send(resp_html)
