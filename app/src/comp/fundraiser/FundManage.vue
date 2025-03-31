f21cf7526762db56e3558ec9b2345f4b67ae04af
shadow-light <noreply@gracious.tech>
2025-03-31 17:36:50 +1100

    Punctuation

--- app/src/comp/fundraiser/FundManage.vue
+++ app/src/comp/fundraiser/FundManage.vue
@@ -2,7 +2,7 @@
 <template lang='pug'>
 
 p
-    | If you have already setup recurring donations, you can cancel them yourself at any time:
+    | If you have already setup recurring donations, you can cancel them yourself at any time.
     |
     span(v-for='manage of fund.payment.manage_recurring')
         a(:href='manage.url' target='_blank') Cancel {{ manage.label }}
