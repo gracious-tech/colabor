1e217d0670afc9b86243585933d58acac8117266
shadow-light <noreply@gracious.tech>
2025-09-25 17:37:30 +1000

    AppAmount

---
+++ app/src/comp/global/AppAmount.vue
@@ -0,0 +1,57 @@
+
+<template lang='pug'>
+
+div.amount(class='d-flex align-center')
+    VTextField(v-model='editable_amount' max-width='200' label='Amount' class='mr-3'
+        @change='on_amount_change')
+    //- NOTE Not limiting to 3 chars since crypto etc may require more
+    VCombobox(v-model='editable_currency' :items='top_currencies' max-width='125' label='Currency'
+        @change='on_currency_change' :readonly='fixed_currency')
+
+</template>
+
+
+<script lang='ts' setup>
+
+import {ref, watch} from 'vue'
+
+import {cents_to_display, display_to_cents, top_currencies} from '@/shared/currency'
+
+
+const {cents, currency, fixed_currency=false}
+    = defineProps<{cents:number, currency:string, fixed_currency:boolean}>()
+const emit = defineEmits(['update:cents', 'update:currency'])
+
+
+// Initial set of editable representations of values (used in fields)
+const editable_currency = ref(currency)
+const editable_amount = ref(cents_to_display(cents, currency))
+
+
+// Watch for changes to props and re-convert to editable values
+watch(() => currency, () => {
+    editable_currency.value = currency
+    // A change of currency affects how cents is interpreted
+    editable_amount.value = cents_to_display(cents, currency)
+})
+watch(() => cents, () => {
+    editable_amount.value = cents_to_display(cents, currency)
+})
+
+
+function on_currency_change(){
+    emit('update:currency', editable_currency.value)
+}
+
+
+function on_amount_change(){
+    emit('update:cents', display_to_cents(editable_amount.value, editable_currency.value))
+}
+
+</script>
+
+
+<style lang='sass' scoped>
+
+
+</style>
